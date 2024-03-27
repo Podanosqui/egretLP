6f9756ffa70dc141e718223a04af8bc2dfb14c85
Podanosqui <rodrigohenrique2210@gmail.com>
2024-03-27 15:30:20 -0300

    Update Image

--- Egret/src/assets/components/howWeAct/HowWeAct.tsx
+++ Egret/src/assets/components/howWeAct/HowWeAct.tsx
@@ -1,6 +1,7 @@
 import { Stack, Typography, useMediaQuery } from "@mui/material";
 import BackgroundHands from "../../images/BackgroundHands.png";
-import Hands from "../../images/MaosImg.png"
+import Hands from "../../images/MaosImg.png";
+import RazaoEmocao from "../../images/RazaoEmocao.png"
 
 export default function HowWeAct() {
 
@@ -37,9 +38,14 @@ export default function HowWeAct() {
                         </Typography>
                     </Stack>
                     <Stack width="100%" mb={2} >
-                        <img src={Hands} alt="Logo Egret" />
+                        {isTablet &&
+                            <img src={Hands} alt="Mãos Razão e Emoção" />
+                        }
+                        {!isTablet &&
+                            <img src={RazaoEmocao} alt="Mãos Razão e Emoção" />
+                        }
                     </Stack>
-                    <Stack sx={isTablet ? { margin: "0 6rem 4rem 6rem", textAlign: "center", color: "#fff" } : {textAlign: "center", color: "#fff", margin: "1rem"}}>
+                    <Stack sx={isTablet ? { margin: "0 6rem 4rem 6rem", textAlign: "center", color: "#fff" } : { textAlign: "center", color: "#fff", margin: "1rem" }}>
                         <Typography fontSize="22px" mb={3}>É natural o empresário agir com emoção afrente da sua empresa.</Typography>
                         <Typography fontSize="22px" mb={3}>Ao tomar decisões, sentimentos de esperança/medo, otimismo/pessimismo, orgulho, frustração, apego, afetam a assertividade das decisões tomadas.</Typography>
                         <Typography fontSize="22px" mb={3}>Nós trazemos uma visão “externa” ao negócio, se concentrando nos indicadores e na experiência acumulada para orientar o processo decisório.</Typography>
