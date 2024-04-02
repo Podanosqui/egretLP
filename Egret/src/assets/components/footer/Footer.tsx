46f83ca85a0c77acabe7972f66fd921dbcf63154
Podanosqui <rodrigohenrique2210@gmail.com>
2024-04-02 18:48:59 -0300

    Update Footer

--- Egret/src/assets/components/footer/Footer.tsx
+++ Egret/src/assets/components/footer/Footer.tsx
@@ -1,4 +1,5 @@
 import { Link, Stack, Typography, useMediaQuery } from "@mui/material";
+import PlaceIcon from '@mui/icons-material/Place';
 
 export default function Footer() {
 
@@ -8,8 +9,48 @@ export default function Footer() {
         <>
             <Stack bgcolor="#07263c" padding="2rem 1rem" justifyContent="space-around">
                 <Stack alignItems="center" mt={3}>
-                    <Typography sx={isTablet ? {} : { mb: 2 }} textAlign="center" color="#fff">&copy; Copyright - 2024, Egret Negócios. Todos os direitos reservados</Typography>
-                    <Typography textAlign="center" color="#fff">Desenvolvido por <Link sx={{ textDecoration: "none", color: "#fff", textTransform: "none", "&:hover": { textDecoration: "underline" } }} href="https://www.linkedin.com/in/rodrigo-podanosqui-939706230/" target='_blank'>Rodrigo Podanosqui</Link></Typography>
+
+                    <Typography
+                    sx={{ mb: 1 }}
+                        textAlign="center"
+                        color="#fff">
+                        <Link
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                textDecoration: "none",
+                                color: "#fff",
+                                textTransform: "none",
+                                "&:hover": {
+                                    textDecoration: "underline"
+                                }
+                            }}
+                            href="https://maps.app.goo.gl/w6trabDK85kvXrK47"
+                            target="_blank">
+                            <PlaceIcon />Avenida das Américas, 500, Bloco 09 | Sala 129 | Barra da Tijuca, Rio de Janeiro | RJ
+                        </Link>
+                    </Typography>
+                    <Typography
+                        sx={{ mb: 2 }}
+                        textAlign="center"
+                        color="#fff">
+                        <Link
+                            sx={{
+                                display: "flex",
+                                alignItems: "center",
+                                textDecoration: "none",
+                                color: "#fff",
+                                textTransform: "none",
+                                "&:hover": {
+                                    textDecoration: "underline"
+                                }
+                            }}
+                            href="https://maps.app.goo.gl/nijLVegGkBvYgcY27"
+                            target="_blank">
+                            <PlaceIcon />Avenida Dom Pedro II, 1.641, Sala 104 | Bairro Jardim, Santo André | SP
+                        </Link>
+                    </Typography>
+                    <Typography sx={{ mt: 5, mb: 5 }} textAlign="center" color="#fff">&copy; Copyright - 2024, Egret Negócios. Todos os direitos reservados</Typography>
                 </Stack>
             </Stack>
         </>
